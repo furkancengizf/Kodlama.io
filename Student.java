8a672cf38cdd16d882d1b51768f2f7a7fd2e3c2c
furkancengizf <78604026+furkancengizf@users.noreply.github.com>
2021-04-30 18:55:51 +0300

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,27 @@
+package Kodlama.io;
+
+  public class Student extends User {
+      private int succesScor;
+      private int numberOfcoursecompleted;
+      
+      public Student(int id, String name, int age, String email){
+        super(id, name, age, email)
+        this.succesScor = succesScor;
+        this.numberofcoursecompleted = numberOfcoursecompleted;
+        
+      
+      }
+      
+      public int getsuccesScore;(){
+        return succesScore
+      }
+      public int setsuccesScore(){
+        this.succesScore = succesScore
+      }
+      public int getnumberOfcoursecomplete(){
+        return numberOfcoursecomplete
+      }
+      public int getnumberOfcoursecomplete(){
+        this.numberOfcoursecomplete = numberOfcoursecomplete
+      }
+  }
